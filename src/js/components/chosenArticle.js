32943b656bf90b985b9dfb79fdd5ecccc169b229
Ma6upa <yuvalerka@mail.ru>
2022-05-16 23:21:03 +0400

    Add mobx on chosenArticle component

--- src/js/components/chosenArticle.js
+++ src/js/components/chosenArticle.js
@@ -1,10 +1,11 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Text } from 'react-native';
+import currentElement from '../store/currentElement';
 
 const ChosenArticle = observer(() => {
   return (
-    <Text>Chosen Article component!</Text>
+    <Text>{currentElement.chosenArticle.title}</Text>
   );
 });
 
